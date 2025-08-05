a60fa3a95417a01249128f4267b59fc74cba1737
steven <steveniot@outlook.com>
2025-08-05 14:27:45 +0800

    Implement CRUD operations with D1 database

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,8 @@ const app = new Hono();
 // Important! No trailing slash
 const allowedOrigins = new Set([
   "http://localhost:5173",
-  "https://<your-frontend-domain>.<tld>",
+  "https://blazenote-frontend-274.pages.dev",
+  "https://blazenote.steventech.cloud"
 ]);
 
 // https://hono.dev/docs/middleware/builtin/cors
